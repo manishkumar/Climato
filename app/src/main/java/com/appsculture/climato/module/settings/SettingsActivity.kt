93f9f13f2b9241e910d4160a32f66ee5963b26d5
jeetu <jeetu.pal@gmail.com>
2018-11-09 17:46:41 +0530

    toolbar implementation in detail and setting screen

--- app/src/main/java/com/appsculture/climato/module/settings/SettingsActivity.kt
+++ app/src/main/java/com/appsculture/climato/module/settings/SettingsActivity.kt
@@ -3,12 +3,14 @@ package com.appsculture.climato.module.settings
 import android.os.Bundle
 import android.preference.PreferenceFragment
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.appsculture.climato.R
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         fragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment())
             .commit()
     }
@@ -18,6 +20,15 @@ class SettingsActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             addPreferencesFromResource(R.xml.pref_settings)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item!!.itemId
+        if (id == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
